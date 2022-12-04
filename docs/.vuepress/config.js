691079650956030f4d896d5b703e6e9431cb2066
我可真猛啊 <j.wors@outlook.com>
2022-12-04 21:58:08 +0800

    部署github

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: '羊雪',
+    description: '羊雪博客',
+    theme: 'reco',
+    themeConfig: {
+        nav: [{
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '羊雪的 JavaScript 博客',
+                items: [{
+                    text: 'Github',
+                    link: 'https://github.com/jwors'
+                }]
+            }
+        ]
+    }
+}
